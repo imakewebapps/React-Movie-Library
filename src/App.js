e566ca9973f190ec70ce0bf0841c4d6655d92d4c
Waqas Yousaf <iamwiqi@gmail.com>
2022-09-18 04:41:20 +0500

    State,Hooks, Props,JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello World!</p>
-    </div>
-  );
+
+const  Person = (props) => {
+  console.log(props);
+  return(
+    <>
+      <h1>fname : {props.data.name}</h1>
+      <h2>lname : {props.data.lname} </h2>
+      <h3>Age : {props.data.age} </h3>
+      </>
+  )
 }
 
+function App() {
+  const [counter, setCounter] =  useState(0);
+  const person = {name : "John", lname:"Doe", age : 36};
+  useEffect(()=>{
+    console.log("Counter"+counter)
+  },[counter])
+
+    return (
+      <div className="App">
+        <Person data={person} />
+        <button onClick={()=> setCounter((prevCount)=> prevCount-1)}>-</button>
+        <h1>{counter}</h1>
+        <button onClick={()=>setCounter((nextState)=> nextState+1)}>+</button>
+      </div>
+    );
+  }
 export default App;
